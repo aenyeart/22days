1227beb1945f1d53ad146ec21d48f618b7167bed
Gediminas <4046456+gedimin45@users.noreply.github.com>
2023-02-26 18:11:37 -0800

    Implemented Submit button

--- components/Input.js
+++ components/Input.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Text, TextInput, View, Button, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
-function Counter({ count, latestMaxPullUps, dispatch }) {
+function Counter({ count, currentFieldValue, latestMaxPullUps, dispatch }) {
   return (
     <View>
       <TextInput keyboardType="numeric"
         style={styles.input}
-        onChangeText={(fieldValue) => dispatch({ type: 'SET_CURRENT_MAX_PULL_UPS', value: fieldValue })}
+        value={currentFieldValue}
+        onChangeText={(fieldValue) => dispatch({ type: 'INPUT_CURRENT_MAX_PULL_UPS', value: fieldValue })}
         placeholder="Hahaha"
       ></TextInput>
+      <Button title="Submit" onPress={() => dispatch({ type: 'SET_CURRENT_MAX_PULL_UPS' })}></Button>
       <Text>Latest max pull-ups: {latestMaxPullUps}</Text>
       <Text>Count: {count}</Text>
       <Button title="Increment" onPress={() => dispatch({ type: 'INCREMENT' })} />
@@ -30,6 +32,7 @@ const styles = StyleSheet.create({
 function mapStateToProps(state) {
   return {
     count: state.count,
+    currentFieldValue: state.currentFieldValue,
     latestMaxPullUps: state.latestMaxPullUps,
   };
 }
--- store.js
+++ store.js
@@ -1,15 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = { count: 0, latestMaxPullUps: null };
+const initialState = { count: 0, currentFieldValue: '', latestMaxPullUps: null };
 
 function reducer(state = initialState, action) {
+  console.log(action);
+
   switch (action.type) {
     case 'INCREMENT':
-      return { count: state.count + 1 };
+      return { ...state, count: state.count + 1 };
     case 'DECREMENT':
-      return { count: state.count - 1 };
+      return { ...state, count: state.count - 1 };
+    case 'INPUT_CURRENT_MAX_PULL_UPS':
+      return { ...state, currentFieldValue: action.value };
     case 'SET_CURRENT_MAX_PULL_UPS':
-      return { latestMaxPullUps: action.value };
+      return { ...state, latestMaxPullUps: state.currentFieldValue, currentFieldValue: '' };
     default:
       return state;
   }
