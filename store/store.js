2b6b568ea3b2b850c8e3799e341c6ed648272fdf
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-05-17 22:23:50 -0700

    Stores all action types as constants in actions.js

--- store/store.js
+++ store/store.js
@@ -1,8 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE,} from 'redux-persist'
+import { configureStore } from '@reduxjs/toolkit';
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE, } from 'redux-persist'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {SET_NEW_AMRAP, SET_NEW_MTF} from "./actions";
-
+import {
+  TEST_DAYS,
+  CLOSE_REPORT,
+  INCREMENT,
+  DECREMENT,
+  SET_INITIAL_STATS,
+  SET_NEW_MTF,
+  SET_NEW_AMRAP,
+  SET_LATEST_AMRAP,
+  SET_FINAL_MAX_PULL_UPS,
+  SET_FINAL_AMRAP,
+  SET_CURRENT_SCAP_HANG,
+  SET_TEST_DAY_TOTAL,
+  SET_LATEST_SCAP_HANG,
+  COMPLETE_TEST,
+  START_NEW_CYCLE,
+  SAVE_RESULTS
+} from "./actions";
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
@@ -27,46 +43,50 @@ function reducer(state = initialState, action) {
   console.log(action.type);
 
   switch (action.type) {
-    case 'TEST_DAYS':
-      return {...state, today: 21};
-    case 'CLOSE_REPORT':
-      return {...state, renderReport: false};
-    case 'INCREMENT':
+    case TEST_DAYS:
+      return { ...state, today: 21 };
+    case CLOSE_REPORT:
+      return { ...state, renderReport: false };
+    case INCREMENT:
       if (state.today === 22) return state; // put logic for report here?
       return { ...state, today: state.today + 1 };
-    case 'DECREMENT':
+    case DECREMENT:
       if (state.today === 1) return state;
       return { ...state, today: state.today - 1 };
-    case 'SET_INITIAL_STATS':
+    case SET_INITIAL_STATS:
       return {
         ...state,
         latestMaxPullUps: state.newMtf,
         latestAmrap: state.newAmrap,
         initialMaxPullUps: state.newMtf,
         initialAmrap: state.newAmrap,
-        testDayTotal: state.newAmrap + state.newMtf,}
+        testDayTotal: state.newAmrap + state.newMtf,
+      }
     case SET_NEW_MTF:
       return { ...state, newMtf: action.value };
     case SET_NEW_AMRAP:
       return { ...state, newAmrap: action.value };
-    case 'SET_FINAL_MAX_PULL_UPS':
+    case SET_LATEST_AMRAP:
+      console.log('latestAmrap updating to', action.value);
+      return { ...state, latestAmrap: action.value };
+    case SET_FINAL_MAX_PULL_UPS:
       return { ...state, finalMaxPullUps: action.value };
-    case 'SET_FINAL_AMRAP':
-      console.log("val passed to finalAmrap", state.latestAmrap);
+    case SET_FINAL_AMRAP:
+      console.log("val passed to finalAmrap", state.latestAmrap); // intent to receive value present in input field
       return { ...state, finalAmrap: state.latestAmrap };
-    case 'SET_CURRENT_SCAP_HANG':
+    case SET_CURRENT_SCAP_HANG:
       return { ...state, currentScapHang: action.value };
-    case 'SET_TEST_DAY_TOTAL':
+    case SET_TEST_DAY_TOTAL:
       return { ...state, testDayTotal: state.newAmrap + state.newMtf }; // Should this be computed in the component?
-    case 'SET_LATEST_SCAP_HANG':
+    case SET_LATEST_SCAP_HANG:
       return { ...state, latestScapHang: state.currentScapHang };
-    case 'COMPLETE_TEST':
+    case COMPLETE_TEST:
       return {
         ...state,
         latestMaxPullUps: state.newMtf,
         latestAmrap: state.newAmrap,
       };
-    case 'START_NEW_CYCLE':
+    case START_NEW_CYCLE:
       return {
         ...state,
         today: 0,
@@ -76,7 +96,7 @@ function reducer(state = initialState, action) {
         currentScapHang: null,
         renderReport: false,
       };
-    case 'SAVE_RESULTS':
+    case SAVE_RESULTS:
       return {
         ...state,
         renderReport: true,
@@ -87,7 +107,7 @@ function reducer(state = initialState, action) {
           mtfDelta: state.finalMaxPullUps - state.initialMaxPullUps,
           initialAmrap: state.initialAmrap,
           finalAmrap: state.finalAmrap,
-          amrapDelta : state.initialAmrap - state.finalAmrap,
+          amrapDelta: state.initialAmrap - state.finalAmrap,
         }]
       };
     case PURGE:
