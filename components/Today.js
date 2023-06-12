6e12347e01ee09476b1de2a370dce54ca6c05d44
gedimin45 <4046456+gedimin45@users.noreply.github.com>
2023-06-11 20:51:46 -0700

    Added initial version of Previous Workout button

--- components/Today.js
+++ components/Today.js
@@ -4,11 +4,18 @@ import {connect} from 'react-redux';
 import styles from "../styles/styles.js";
 import TodaysWorkout from './TodaysWorkout.js';
 import {Text} from "./Text.js";
+import {Button, Icon} from "@rneui/themed";
 
 function Today({ today }) {
   return (
     <KeyboardAvoidingView behavior="padding" style={{ width: "100%" }}>
       <ScrollView contentContainerStyle={{ width: "100%" }}>
+        <Button type="outline"
+                containerStyle={{width: '60%'}}
+                buttonStyle={{ justifyContent: 'flex-start' }}>
+          <Icon name="back-in-time" type="entypo" color="white" />
+          <Text>Previous Workout</Text>
+        </Button>
             <Text style={styles.title}>
               This is workout #{today}:
             </Text>
