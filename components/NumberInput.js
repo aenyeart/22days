f5339f951581a0e2bd2755b854b9e80480da695e
gedimin45 <4046456+gedimin45@users.noreply.github.com>
2023-06-28 21:00:59 -0700

    Style NumberInput

--- components/NumberInput.js
+++ components/NumberInput.js
@@ -4,6 +4,11 @@ import InputSpinner from "react-native-input-spinner";
 export default ({ ...props }) => {
   return (
     <InputSpinner
+      background={"#FFFFFF"}
+      colorLeft={"#FF9595"}
+      colorRight={"#3FE180"}
+      fontSize={28}
+      style={{padding: 8, borderRadius: 40}}
       min={0}
       step={1}
       value={props.initialValue}
