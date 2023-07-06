d7b47926550d7ac7644cf26f2e7cd45a4da06c86
gedimin45 <4046456+gedimin45@users.noreply.github.com>
2023-07-05 21:15:58 -0700

    Add some style to the Previous Workout button

--- components/Today.js
+++ components/Today.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import {KeyboardAvoidingView, ScrollView} from 'react-native';
+import {KeyboardAvoidingView, Pressable, ScrollView} from 'react-native';
 import {connect} from 'react-redux';
 import styles from "../styles/styles.js";
 import TodaysWorkout from './TodaysWorkout.js';
 import {Text} from "./Text.js";
-import {Button, Icon} from "@rneui/themed";
+import {Icon} from "@rneui/themed";
 
 function Today({ today, dispatch }) {
   return (
     <KeyboardAvoidingView behavior="padding" style={{ width: "100%" }}>
       <ScrollView contentContainerStyle={{ width: "100%" }}>
-        <Button type="outline"
-                containerStyle={{width: '60%'}}
-                buttonStyle={{ justifyContent: 'flex-start' }}
-                onPress={() => dispatch({ type: 'DECREMENT' })}
+        <Pressable type="outline"
+                   style={{
+                     width: '60%',
+                     flexDirection: "row",
+                     alignItems: "center",
+                   }}
+                   onPress={() => dispatch({type: 'DECREMENT'})}
         >
-          <Icon name="back-in-time" type="entypo" color="white" />
-          <Text>Previous Workout</Text>
-        </Button>
+          <Icon name="back-in-time" type="entypo" color="white" size={24} style={{
+              borderStyle: 'solid', borderRadius: 30, borderWidth: 1, borderColor: 'white', padding: 12,
+              marginRight: 10,
+          }} />
+          <Text style={{fontWeight: "600"}}>Previous Workout</Text>
+        </Pressable>
             <Text style={styles.title}>
               This is workout #{today}:
             </Text>
