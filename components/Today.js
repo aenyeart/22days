82287d59d7905b2c2f57357f1e6209efc907025f
gedimin45 <4046456+gedimin45@users.noreply.github.com>
2023-07-26 21:55:31 -0700

    Use React Native Elements Header in Today.js

--- components/Today.js
+++ components/Today.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { KeyboardAvoidingView, Pressable, ScrollView, StyleSheet, View, Dimensions } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
 import { connect } from 'react-redux';
 import styles from "../styles/styles.js";
 import TodaysWorkout from './TodaysWorkout.js';
 import { Text } from "./Text.js";
 import { Icon } from "@rneui/themed";
+import {Header} from "@rneui/base";
 
 
 function Today({ today, dispatch }) {
@@ -18,7 +24,6 @@ function Today({ today, dispatch }) {
       marginTop: .05 * height,
     },
     inner: {
-      // flex: 1,
       marginHorizontal: 'auto',
       width: "80%",
     },
@@ -26,48 +31,41 @@ function Today({ today, dispatch }) {
 
   return (
     <>
-      <View style={{
-        width: '100%',
-        opacity: 0.3,
-        backgroundColor: '#FFFFFF',
-        borderBottomRightRadius: 35,
-        // height: 100,
-        paddingRight: 3,
-        height: .15 * height,
-        position: 'absolute',
-        top: 0,
-        right: 0,
-      }} />
 
-      <View style={{
-        height: .07 * height,
-        width: '100%',
-        flexDirection: "row",
-        // alignItems: "center",
-        justifyContent: 'flex-start',
-        marginLeft: "10%",
-        // paddingBottom: 10,
-      }}>
-        <Pressable type="outline"
-          style={{
-            width: '60%',
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-          onPress={() => dispatch({ type: 'DECREMENT' })}
-        >
-          <Icon name="back-in-time" type="entypo" color="white" size={24} style={{
-            borderStyle: 'solid',
-            borderRadius: '100%',
-            borderWidth: 1,
-            borderColor: 'rgba(255, 255, 255, .5)',
-            padding: 12,
-            marginRight: 10,
-          }} />
-          <Text style={{ fontWeight: "600" }}>Previous Workout</Text>
-        </Pressable>
-      </View>
-      {/* <KeyboardAvoidingView behavior="padding" style={localStyles.cincher}> */}
+      <Header
+        containerStyle={{
+          paddingHorizontal: "10%",
+          borderBottomWidth: 0,
+          borderBottomRightRadius: 35,
+          backgroundColor: "rgba(255, 255, 255, .3)",
+        }}
+
+        placement={'left'}
+        leftContainerStyle={{flex: 0, width: 0}}
+        rightContainerStyle={{flex: 0, width: 0}}
+
+        centerContainerStyle={{paddingHorizontal: 0}}
+        centerComponent={
+          <Pressable type="outline"
+                     style={{
+                       width: '60%',
+                       flexDirection: "row",
+                       alignItems: "center",
+                     }}
+                     onPress={() => dispatch({ type: 'DECREMENT' })}
+          >
+            <Icon name="back-in-time" type="entypo" color="white" size={24} style={{
+              borderStyle: 'solid',
+              borderRadius: '100%',
+              borderWidth: 1,
+              borderColor: 'rgba(255, 255, 255, .5)',
+              padding: 12,
+              marginRight: 10,
+            }} />
+            <Text style={{ fontWeight: "600" }}>Previous Workout</Text>
+          </Pressable>
+        }
+      />
 
       <View style={localStyles.outer}>
         <View style={localStyles.inner}>
