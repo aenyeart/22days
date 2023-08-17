c90d22e9021ee335e686f2fea52406214be668eb
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-08-16 20:59:56 -0700

    Moves styles to end of component

--- components/CompleteButton.js
+++ components/CompleteButton.js
@@ -2,14 +2,6 @@ import { Pressable, StyleSheet } from 'react-native';
 import { Text } from './Text.js';
 import { connect } from 'react-redux';
 
-const localStyles = StyleSheet.create({
-  backgroundColor: '#6641AF',
-  width: "100%",
-  // flex: 1,  // NOTE: this is what was causing the text to not show up
-  padding: 15,
-  alignItems: 'center',
-});
-
 function CompleteButton({ action, dispatch, title = "Complete Workout" }) {
   return (
     <Pressable
@@ -22,8 +14,17 @@ function CompleteButton({ action, dispatch, title = "Complete Workout" }) {
     </Pressable>
   )
 }
+
 function mapStateToProps(state) {
   return { ...state };
 }
 
 export default connect(mapStateToProps)(CompleteButton);
+
+const localStyles = StyleSheet.create({
+  backgroundColor: '#6641AF',
+  width: "100%",
+  // flex: 1,  // NOTE: this is what was causing the text to not show up
+  padding: 15,
+  alignItems: 'center',
+});
