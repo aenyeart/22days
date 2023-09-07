a63d61b47ad7a8af00010276162299574c8d1ac6
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-09-06 21:15:05 -0700

    Demonstrates new approach to vertical timeline

--- components/workouts/InitialTest.js
+++ components/workouts/InitialTest.js
@@ -1,13 +1,29 @@
 import { Text } from '../Text.js';
 import Input from '../NumberInput.js';
 import CompleteButton from '../CompleteButton.js';
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { View } from 'react-native';
+
 
 export default ({ style, mtf, latestAmrap, today }) => {
   const [newMtf, setNewMtf] = useState(mtf);
   const [newAmrap, setNewAmrap] = useState(latestAmrap);
+  const viewRef = useRef(null);
+
+  useEffect(() => {
+    // Ensure the view has been rendered before measuring
+    if (viewRef.current) {
+      viewRef.current.measure((x, y, width, height, pageX, pageY) => {
+        console.log('X coordinate:', x);
+        console.log('Y coordinate:', y);
+        console.log('Width:', width);
+        console.log('Height:', height);
+        console.log('Page X coordinate:', pageX);
+        console.log('Page Y coordinate:', pageY);
+      });
+    }
+  }, []);
 
   return (
     <View style={style.outer}>
@@ -24,7 +40,19 @@ export default ({ style, mtf, latestAmrap, today }) => {
         <Text>
           2-min rest
         </Text>
-        <Text>Do pull-ups for AMRAP(i) in 5-min. {`\n`}Breaks allowed, but timer must not stop:</Text>
+        <View ref={viewRef}>
+          <View style={{
+            width: 15,
+            height: 15,
+            backgroundColor: '#D5FF2C',
+            position: 'absolute',
+            left: -20,
+            top: 20,
+            borderRadius: 50
+
+            }}></View>
+          <Text>Do pull-ups for AMRAP(i) in 5-min. {`\n`}Breaks allowed, but timer must not stop:</Text>
+        </View>
         <Input
           initialValue={latestAmrap}
           handleChange={setNewAmrap}
