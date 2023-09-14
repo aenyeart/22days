270723733c2f4a9ed8f08c754368476f3450297e
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-09-13 21:47:18 -0700

    Cleans unused code and outdated comments

--- components/workouts/InitialTest.js
+++ components/workouts/InitialTest.js
@@ -1,9 +1,8 @@
 import { Text } from '../Text.js';
 import Input from '../NumberInput.js';
 import CompleteButton from '../CompleteButton.js';
-import { useState, useRef, useEffect } from "react";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { View, Dimensions } from 'react-native';
+import { useState } from "react";
+import { View } from 'react-native';
 import TimelineNode from '../TimelineNode.js';
 import Timeline from '../Timeline.js';
 
@@ -11,28 +10,13 @@ import Timeline from '../Timeline.js';
 export default ({ style, mtf, latestAmrap, today }) => {
   const [newMtf, setNewMtf] = useState(mtf);
   const [newAmrap, setNewAmrap] = useState(latestAmrap);
-  const viewRef = useRef(null);
-
-  useEffect(() => {
-    // Ensure the view has been rendered before measuring
-    if (viewRef.current) {
-      viewRef.current.measure((x, y, width, height, pageX, pageY) => {
-        // console.log('X coordinate:', x);
-        // console.log('Y coordinate:', y);
-        // console.log('Width:', width);
-        // console.log('Height:', height);
-        // console.log('Page X coordinate:', pageX);
-        // console.log('Page Y coordinate:', pageY);
-      });
-    }
-  }, []);
 
   return (
     <View style={style.outer}>
       {/* GREEN */}
       <View style={style.inner}>
         {/* PINK */}
-        <View ref={viewRef}>
+        <View>
           <TimelineNode />
           <Timeline />
           <Text style={{ paddingLeft: 8 }}>
@@ -43,17 +27,15 @@ export default ({ style, mtf, latestAmrap, today }) => {
             handleChange={setNewMtf}
           />
         </View>
-        <View ref={viewRef}>
+        <View>
           <TimelineNode />
-          <Timeline />
           <Text style={{ paddingLeft: 8 }}>
             2-min rest
           </Text>
         </View>
 
-        <View ref={viewRef}>
+        <View>
           <TimelineNode />
-          <Timeline />
           <Text style={{ paddingLeft: 8 }}>
             Do pull-ups for AMRAP(i) in 5-min. {`\n`}Breaks allowed, but timer must not stop:
           </Text>
