57affec0937ab9f4cafafa153d65ce1a47735350
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-09-25 16:30:11 -0700

    Fine-tunes the centering on PreviousWorkout

--- components/Today.js
+++ components/Today.js
@@ -36,25 +36,27 @@ function Today({ today, latestMaxPullUps, latestAmrap, latestScapHang, testDayTo
               display: 'flex',
               flexDirection: 'row',
               marginTop: 5,
+              alignItems: 'center',
             }}>
             <Pressable
               onPress={() => dispatch({ type: 'DECREMENT' })}
-              style={{ position: 'absolute', left: -50, padding: 10}}>
+              style={{
+                position: 'absolute',
+                left: -50,
+                padding: 11
+              }}>
               <Icon name="chevron-left" color="white" />
             </Pressable>
             <Pressable
-              onPress={() => dispatch({ type: 'DECREMENT' })}
-              style={{
-                flexDirection: 'row',
-              }}
-              >
+              onPress={() => dispatch({ type: 'DECREMENT' })}>
               <Text style={{
                 fontWeight: "600",
                 fontSize: 20,
-                paddingVertical: 12,
+                paddingVertical: 11,
               }}>
                 Previous Workout
               </Text>
+            {/* <View style={{height: 10, width: '100%'}}/> */}
             </Pressable>
           </View>
         }
