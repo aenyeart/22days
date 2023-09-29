0e85d0156dcc9400269c6d1a93b8635b87c23384
Andrew Enyeart <reallyhardtoforget@gmail.com>
2023-09-29 14:27:46 -0700

    Remove Report from Home

--- components/Home.js
+++ components/Home.js
@@ -14,31 +14,24 @@ function Home({ today, renderReport, initialMaxPullUps, initialAmrap, finalMaxPu
   return (
     <>
       <StatusBar style="auto" />
-      <View style={styles.outer}>
-        {renderReport ?
-          <Report
-            style={styles.container}
-            initialMaxPullUps={initialMaxPullUps}
-            initialAmrap={initialAmrap}
-            finalMaxPullUps={finalMaxPullUps}
-            finalAmrap={finalAmrap}
-          /> :
-          <>
-            {/* {today === 1 && <Text style={styles.menuItem}>{messages.beginning}</Text>} */}
-            <Today style={styles.container} />
-            {/* <DevMenu /> */}
-          </>
-        }
-      </View>
+      <Today style={styles.container} />
+      {/* <Report
+        style={styles.}
+        initialMaxPullUps={initialMaxPullUps}
+        initialAmrap={initialAmrap}
+        finalMaxPullUps={finalMaxPullUps}
+        finalAmrap={finalAmrap}
+      /> */}
+      {/* <DevMenu /> */}
       {/*
         - One quick way to get the complete button all the way at the bottom at full width is to place it beneath the SafeAreaProvider. (This button could be placed in the Today component, but I'm not sure how to get it to the bottom of the screen at full width yet.)
         - Another way would be to place it in the Today component, and then use flexbox to push it to the bottom of the screen. (This would be a good way to learn flexbox.)
         - Another way would be to place it in the Today component, and then use absolute positioning to push it to the bottom of the screen. (This would be a good way to learn absolute positioning.)
       */}
-      {/* TODO: Add logic to conditionally render CompleteButton on all days except the report view.
+      {/*
+      TODO: Add logic to conditionally render CompleteButton on all days except the report view.
       Perhaps the report view could be a modal that pops up when the user clicks the CompleteButton on the last day.
       */}
-      {/* <CompleteButton /> */}
     </>
   );
 }
@@ -49,9 +42,6 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps)(Home);
 
 const styles = StyleSheet.create({
-  outer: {
-
-  },
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
