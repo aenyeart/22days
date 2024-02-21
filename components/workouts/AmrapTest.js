66d99cdc2d9692a78f5ed47c8d9305285ea325e7
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-02-20 23:25:40 -0800

    Completes scrollable layout for Amrap

--- components/workouts/AmrapTest.js
+++ components/workouts/AmrapTest.js
@@ -39,18 +39,29 @@ export default ({ tdt, latestAmrap, today }) => {
         </Text>
         <Divider style={{ width: "100%", marginBottom: 20 }} />
 
-        <View>
-          <TimelineNode />
-          <Timeline />
-          <Text style={workoutStyles.text}>Test – Do pull-ups for AMRAP(i) within a 5-min period:
-            {`\n`}– Breaks are allowed, but the timer must not stop.
-            {`\n`}– Try to beat {tdt} reps, your total pull-ups from Day 1.
-          </Text>
+        <View style={{
+          flexDirection: 'row',
+        }}
+          onLayout={getHeightForTimeline}
+        >
+          <Timeline style={{ height: this.workoutHeight }} />
 
-          <Input
-            initialValue={newAmrap}
-            handleChange={setNewAmrap}
-          />
+          <View style={{}}>
+
+            <View>
+              <TimelineNode />
+              <Timeline />
+              <Text style={workoutStyles.text}>Test – Do pull-ups for AMRAP(i) within a 5-min period:
+                {`\n`}– Breaks are allowed, but the timer must not stop.
+                {`\n`}– Try to beat {tdt} reps, your total pull-ups from Day 1.
+              </Text>
+
+              <Input
+                initialValue={newAmrap}
+                handleChange={setNewAmrap}
+              />
+            </View>
+          </View>
         </View>
       </ScrollView>
       <CompleteButton action={{ type: "SET_FINAL_AMRAP", value: newAmrap }} />
