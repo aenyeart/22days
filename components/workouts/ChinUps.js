87bb6fe3ff6ca4c402daf39f862e87ea4f8ca091
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-02-20 20:55:15 -0800

    Adds today header and divider

--- components/workouts/ChinUps.js
+++ components/workouts/ChinUps.js
@@ -1,19 +1,41 @@
 import CompleteButton from '../CompleteButton.js';
 import { Text } from "../Text.js";
-import { View } from 'react-native';
+import { useState } from "react";
+import { View, ScrollView } from 'react-native';
 import TimelineNode from "../TimelineNode";
 import Timeline from "../Timeline";
 import CountdownTimer from "../CountdownTimer";
+import { Divider } from "@rneui/themed";
 import styles from "../../styles/styles.js";
 
-export default ({ mtf, style }) => {
+
+export default ({ mtf, style, today }) => {
+  const [workoutHeight, setWorkoutHeight] = useState(0);
+  const getHeightForTimeline = (event) => {
+    let { height } = event.nativeEvent.layout;
+    height += 20;
+    console.log('height check', height);
+    setWorkoutHeight(height);
+  }
   const workoutStyles = styles.workoutStyles;
 
   return (
     <>
       {/* GREEN */}
-      <View style={workoutStyles.inner}>
+      <ScrollView bounces='true' style={workoutStyles.inner}>
         {/* PINK */}
+        <Text style={{
+          textAlign: 'center',
+          alignSelf: 'center',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 20,
+          width: '80%',
+        }}>
+          Workout #{today}:
+        </Text>
+        <Divider style={{ width: "100%", marginBottom: 20 }} />
 
 
         <View>
@@ -39,7 +61,7 @@ export default ({ mtf, style }) => {
           </Text>
         </View>
 
-      </View>
+      </ScrollView>
       <CompleteButton action={null} />
     </>
   );
