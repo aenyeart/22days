515ec4bde9cb0813c013f70d73f68c58c661397e
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-02-29 20:39:02 -0800

    Add WorkoutStep component to AmrapTest.js

--- components/workouts/AmrapTest.js
+++ components/workouts/AmrapTest.js
@@ -3,7 +3,7 @@ import Input from '../NumberInput.js';
 import { useState } from "react";
 import { Text } from '../Text.js';
 import { View, ScrollView } from 'react-native';
-import TimelineNode from "../TimelineNode";
+import WorkoutStep from '../WorkoutStep.js';
 import Timeline from "../Timeline";
 import { Divider } from "@rneui/themed";
 import styles from "../../styles/styles.js";
@@ -45,12 +45,9 @@ export default ({ tdt, latestAmrap, today }) => {
           onLayout={getHeightForTimeline}
         >
           <Timeline style={{ height: this.workoutHeight }} />
-
-          <View style={{}}>
-
-            <View>
-              <TimelineNode />
-              <Timeline />
+          <View>
+            {/* WRAPPER: Workout Elements */}
+            <WorkoutStep>
               <Text style={workoutStyles.text}>Test – Do pull-ups for AMRAP(i) within a 5-min period:
                 {`\n`}– Breaks are allowed, but the timer must not stop.
                 {`\n`}– Try to beat {tdt} reps, your total pull-ups from Day 1.
@@ -60,7 +57,8 @@ export default ({ tdt, latestAmrap, today }) => {
                 initialValue={newAmrap}
                 handleChange={setNewAmrap}
               />
-            </View>
+            </WorkoutStep>
+
           </View>
         </View>
       </ScrollView>
