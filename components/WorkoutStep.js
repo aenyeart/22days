07aef455ceb8034af604a879df23fc3d102b0ac8
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-05-01 20:07:12 -0700

    Refactor WorkoutStep component to use function declaration and extract styles to a variable

--- components/WorkoutStep.js
+++ components/WorkoutStep.js
@@ -2,9 +2,15 @@ import { View } from 'react-native'
 import React from 'react'
 import TimelineNode from './TimelineNode'
 
-export default ({children}) => {
+export default function WorkoutStep({ children }) {
+  const styles = {
+    width: '98%',
+    paddingBottom: 10,
+    flexGrow: 1
+  };
+
   return (
-    <View style={{ width: '98%', paddingBottom: 10 }}>
+    <View style={styles}>
       <TimelineNode />
       {children}
     </View>
