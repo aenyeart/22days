1b8fc41b64e4914e7317bdd7bbbbfa79d8c49387
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-08-06 16:08:15 -0700

    chore: Update code formatting in App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import {Text} from './components/Text.js';
+import { Text } from './components/Text.js';
 import Home from './components/Home.js';
-import {store, persistor} from './store/store';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
-import {LinearGradient} from "expo-linear-gradient";
+import { store, persistor } from './store/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { LinearGradient } from "expo-linear-gradient";
 import styles from "./styles/styles";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {useEffect} from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useEffect } from "react";
 import * as Permissions from "expo-permissions";
 import * as Notifications from "expo-notifications";
 
@@ -14,7 +14,8 @@ Notifications.setNotificationHandler({
   handleNotification: async () => {
     return {
       shouldShowAlert: true
-    }}
+    }
+  }
 })
 
 export default function App() {
@@ -32,20 +33,22 @@ export default function App() {
   }, [])
 
   return (
-    <SafeAreaProvider>
-    <Provider store={store}>
-      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
-        <LinearGradient
-          colors={['rgba(72, 56, 103, 1)', 'rgba(61, 120, 234, 1)']}
-          start={{x: 0, y: .25}}
-          end={{x: 1, y: .72}}
-          locations={[0.1, 1]}
-          style={styles.background}
-        />
-        <Home />
+    <>
+      <SafeAreaProvider>
+        <Provider store={store}>
+          <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+            <LinearGradient
+              colors={['rgba(72, 56, 103, 1)', 'rgba(61, 120, 234, 1)']}
+              start={{ x: 0, y: .25 }}
+              end={{ x: 1, y: .72 }}
+              locations={[0.1, 1]}
+              style={styles.background}
+            />
+            <Home />
 
-      </PersistGate>
-    </Provider>
-    </SafeAreaProvider>
+          </PersistGate>
+        </Provider>
+      </SafeAreaProvider>
+    </>
   );
 }
