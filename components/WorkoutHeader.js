5cbb800d1c7df3798003c58a950c200bbbd23431
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-08-07 12:55:44 -0700

    Optimize WorkoutHeader to use StyleSheet

--- components/WorkoutHeader.js
+++ components/WorkoutHeader.js
@@ -1,7 +1,8 @@
 import { Text } from './Text.js'
+import { StyleSheet } from 'react-native';
 
 export default function WorkoutHeader({ today }) {
-  const styles = {
+  const headerStyles = StyleSheet.create({
     textAlign: 'center',
     alignSelf: 'center',
     fontSize: 24,
@@ -9,10 +10,10 @@ export default function WorkoutHeader({ today }) {
     marginTop: 10,
     marginBottom: 20,
     width: '80%',
-  };
+  });
 
   return (
-    <Text style={styles}>
+    <Text style={headerStyles}>
       Workout #{today}:
     </Text>
   );
