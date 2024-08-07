72b84c54ba725e4e77905dfebd88d5c8ee36fbc6
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-08-07 12:59:54 -0700

    Removes unused styles

--- components/CompleteButton.js
+++ components/CompleteButton.js
@@ -51,13 +51,9 @@ const localStyles = StyleSheet.create({
     width: '112%',
     position: 'relative',
     left: '-6%', // NOTE: if button loses alignment, adjust this value
-    // bottom: .08 * height,
     borderTopLeftRadius: 45,
   },
   button: {
-    // width: '100%',
     padding: 10,
-    // paddingBottom: .08 * height,
-    // borderTopLeftRadius: 45,
   }
 });
