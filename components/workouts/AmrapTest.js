f91cdfa5019cfd5bb74cc9bd52f59fe1890b6d65
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-08-15 14:38:33 -0700

    Add five-min timer to AmrapTest and InitialTest

--- components/workouts/AmrapTest.js
+++ components/workouts/AmrapTest.js
@@ -1,6 +1,7 @@
 import Input from '../NumberInput.js';
 import { useState } from "react";
 import { Text } from '../Text.js';
+import CountdownTimer from '../CountdownTimer.js';
 import WorkoutStep from '../WorkoutStep.js';
 import styles from "../../styles/styles.js";
 import WorkoutTemplate from "../WorkoutTemplate";
@@ -8,22 +9,28 @@ import WorkoutTemplate from "../WorkoutTemplate";
 export default ({ tdt, latestAmrap, today }) => {
   const [newAmrap, setNewAmrap] = useState(latestAmrap);
   return (
-      <WorkoutTemplate
-        action={{ type: "SET_FINAL_AMRAP", value: newAmrap }}
-        today={today}
-      >
-        <WorkoutStep>
-          <Text style={styles.workoutStyles.text}>Test – Do pull-ups for AMRAP(i) within a 5-min period:
-            {`\n`}– Breaks are allowed, but the timer must not stop.
-            {`\n`}– Try to beat {tdt} reps, your total pull-ups from Day 1.
-          </Text>
-
-          <Input
-            initialValue={newAmrap}
-            handleChange={setNewAmrap}
-          />
+    <WorkoutTemplate
+      action={{ type: "SET_FINAL_AMRAP", value: newAmrap }}
+      today={today}
+    >
+      <WorkoutStep>
+        <Text style={styles.workoutStyles.text}>Test – Do pull-ups for AMRAP(i) within a 5-min period.
+          {`\n`}Breaks are allowed, but the timer must not stop.
+          {`\n`}
+          {`\n`}Try to beat {tdt} reps, your TOTAL pull-ups (MTF + AMRAP) from Day 1!
+        </Text>
+        <CountdownTimer seconds={300} timerText={'5-minute AMRAP test finished. \nRecord your completed reps.'} />
       </WorkoutStep>
-      {/* BUG: timelineLayout.x gets different values (e.g. sometimes 27, or 48) when navigating to this specific workout. Perhaps a race condition needing a setTimeout? */}
+
+      <WorkoutStep>
+        <Text style={styles.workoutStyles.text}>
+          How many pull-ups did you complete in 5 minutes?
+        </Text>
+        <Input
+          initialValue={latestAmrap}
+          handleChange={setNewAmrap}
+        />
+      </WorkoutStep>
     </WorkoutTemplate>
   );
 }
