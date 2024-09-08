7a1a9151d4beed18eb09b0adead84d8a21f72d18
Andrew Enyeart <reallyhardtoforget@gmail.com>
2024-09-08 15:16:46 -0700

    chore: Update NumberInput component to handle empty append prop

--- components/NumberInput.js
+++ components/NumberInput.js
@@ -8,7 +8,7 @@ export default ({ ...props }) => {
 
   return (
     <InputSpinner
-      append={props.append || ''}
+      append={props.append || <></>}
       background={"#FFFFFF"}
       colorLeft={"#FF9595"}
       colorRight={"#3FE180"}
